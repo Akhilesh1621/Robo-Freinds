148aea619263286aa186f9a66f3774a7639420fa
akhilesh <akhileshdevop@gmail.com>
2021-04-23 00:25:30 +0530

    1v

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello  from './Hello';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Hello greeting={'Hello' + ' React Ninja'}/>,
   document.getElementById('root')
 );
 
